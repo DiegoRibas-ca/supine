a9058087b4caf1a3e35b222e04f94ae4d1188a93
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-02-09 21:31:48 +0000

    event handlerswhen fill the field and buttom in order page to redirect to main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,43 @@
-
-
-// $(() => {
-//   $.ajax({
-//     method: "POST",
-//     url: "/order"
-//   }).done((users) => {
-    
-//     for(user of users) {
-//       $("<div>").text(user.name_rest).appendTo($("body"));
-//     }
-//   });;
-// });
-
 //CALCULATE THE TOTAL PRICE WITH JQUERY
+function sucessOrder(data) {
+  console.log(data);
+  $(".menu-container").toggle();
+  $('.order-response').append($("<p id='order-complete'></p>").text(data));
+  $('.order-response').append($("<button id='gobackhome'>Back to Home</button>").val('Back to Home'));
+  $("#gobackhome").click(function () {
+    window.location.replace("http://localhost:8080/");
+  });
+
+}
+let totalPrice = 0;
+let priceHamb = 12;
+let priceSushi = 11;
+let priceCoke = 2;
+let priceJuice = 3;
 
 $(document).ready(function () {
+  
+  $(".item-list").val(0);
 
   $('#orderButton').on('submit', function (event) {
     event.preventDefault();
     let body = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: '/order',
-      data: body,
-      success: sucessOrder
-    });
-
+    console.log($('#user_phone').val().length);
+    if ($('#user_phone').val().length !== 10) {
+      alert("Sorry, your phone number must have 10 digits");
+    } else if ($('#user_name').val() === "" || $('#user_name').val() === null) {
+        alert("Sorry, you must type your name");
+    } else if ($('#total-price').val() < 1) {
+        alert("Sorry, you should make a order first");
+    } else {
+      $.ajax({
+        type: "POST",
+        url: '/order',
+        data: body,
+        success: sucessOrder
+      });
+    }  
   })
-   
-  
-  function sucessOrder(data) {
-    console.log(data);
-    $(".menu-container").toggle();
-    $('.order-response').append($("<p id='order-complete'></p>").text(data))
-    $('.order-response').append($("<button id='#gobackhome'>Back to Home</button>").val('Back to Home'));
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-  let totalPrice = 0;
-  let priceHamb = 12;
-  let priceSushi = 11;
-  let priceCoke = 2;
-  let priceJuice = 3;
-  $(".item-list").val(0);
-
 
   $(".item-list").click(function() {
     totalPrice = 0;
@@ -76,46 +59,5 @@ $(document).ready(function () {
 
 
 
+});
 
-
-
-
-
-
-
-
-
-
-
-  // $(".item-list").click(function () {
-  //   totalPrice = 0;
-  //   totalPrice = + $("#item1").val() * priceHamb;
-  //   totalPrice = + $("#item2").val() * priceSushi;
-  //   totalPrice = + $("#item3").val() * priceCoke;
-  //   totalPrice = + $("#item4").val() * priceJuice;
-  //   $('#total-price').text(totalPrice);
-  // });
-  // $('.item1').on('click', function () {
-  //   // const maxLength = 140;
-  //   let totalQuantity =+ $(this).val()
-  //   $('#total-price').text(totalQuantity);
-  //   alert('changed');
-  // })
-})
-//     let $counter = $(this).siblings('span');
-//     $counter.text(maxLength - length);
-
-//     let characLeft = maxLength - length;
-
-//     if (characLeft < 0) {
-//       $counter.addClass("makeItRed");
-//     } else {
-//       $counter.removeClass("makeItRed");
-//     };
-//   });
-
-// });
-  //MAKE PROMISES AND INSERT TABLES
-// $('.counter').text(140);
-// $(".container .new-tweet").toggle('show');
-// $('.new-tweet textarea').val('');
