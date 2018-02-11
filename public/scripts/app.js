d2001f2548e84500fda57e3ddd07056cf437cf4b
Simon <simon@simonhamilton.ca>
2018-02-11 23:38:05 +0000

    fixed price error, cleaned code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,8 @@
-//CALCULATE THE TOTAL PRICE WITH JQUERY
+
+// after successful order, json data will be returned with 200 status code
+// use this data to build order confirmation and payment page
 function sucessOrder(data) {
-  console.log("price data", data);
+  //console.log("price data", data);
 
   $("#menu-form").toggle();
   $('.main-menu').append($(`
@@ -36,7 +38,7 @@ function sucessOrder(data) {
   </section>
 
   `));
-  // $('.order-response').append($(""));
+// go home button if user does not want to pre pay
   $("#gobackhome").click(function () {
     window.location.replace("http://localhost:8080/");
   });
@@ -46,25 +48,29 @@ function sucessOrder(data) {
 let totalPrice = 0;
 let priceHamb = Math.round((12*1.13) * 100)/100;
 let priceSushi = Math.round((11*1.13) * 100)/100;
-let priceCoke = Math.round((3*1.13) * 100)/100;
-let priceJuice = Math.round((2*1.13) * 100)/100;
+let priceCoke = Math.round((2*1.13) * 100)/100;
+let priceJuice = Math.round((3*1.13) * 100)/100;
 
 $(document).ready(function () {
 
   $(".item-list").val(0);
 
+// On order submit button
   $('#orderButton').on('submit', function (event) {
+    //prevent default form submission, lets control this ourselves with ajax
     event.preventDefault();
     let body = $(this).serialize();
 
+// set error message with empty order fields
     console.log($('#user_phone').val().length);
-    if ($('#user_phone').val().length !== 10) {
-      alert("Sorry, your phone number must have 10 digits");
+    if ($('#total-price').val() < 1){
+      alert("You have failed to meet the $1 minimum order");
     } else if ($('#user_name').val() === "" || $('#user_name').val() === null) {
-        alert("Sorry, you must type your name");
-    } else if ($('#total-price').val() < 1) {
-        alert("Sorry, you should make a order first");
+        alert("Please input your name or Ill call you null!");
+    } else if  ($('#user_phone').val().length !== 10) {
+        alert("Hey you...whats your (real) number");
     } else {
+      //if succesful, send ajax request to POST /order
       $.ajax({
         type: "POST",
         url: '/order',
@@ -74,7 +80,7 @@ $(document).ready(function () {
     }
   })
 
-
+// calc total price via key up or click
 
   $(".item-list").click(function() {
     totalPrice = 0;
